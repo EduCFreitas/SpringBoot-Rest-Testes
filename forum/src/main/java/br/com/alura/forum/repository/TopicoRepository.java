92d251de33d53541674286a55b316d9d6774628d
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-12-16 11:46:13 -0300

    Aula 2.1: Paginação

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,10 +1,8 @@
 package br.com.alura.forum.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.forum.modelo.Topico;
@@ -12,9 +10,6 @@ import br.com.alura.forum.modelo.Topico;
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
-	List<Topico> findByCurso_Nome(String nomeCurso);
+	Page<Topico> findByCurso_Nome(String nomeCurso, Pageable paginacao);
 	
-//	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
-//	List<Topico>carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
-
 }
