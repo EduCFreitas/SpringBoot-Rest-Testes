3cc40d4ff3d51964ea0bd279ae1ec48a2e65b2d9
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-12-16 16:06:09 -0300

    Aula 2.3: Restringindo o acesso aos endpoints privados

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -22,7 +22,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET, "/topicos").permitAll();
+			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.anyRequest().authenticated()
+			.and().formLogin();
 	}
 	
 	//Configurações de recursos estáticos (JS, CSS, imagens etc.)
